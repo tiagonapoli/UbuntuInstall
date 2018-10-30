4e4915c0ad4c99d595eaf8a5a23db9ee7b544e22
tiagonapoli <napoli.tiago@hotmail.com>
2018-10-30 08:29:27 -0300

    upd

--- packages_install.sh
+++ packages_install.sh
@@ -77,25 +77,27 @@ then
 fi
 
 #git
-#sudo apt-get -y install git
-#git config --global user.email "napoli.tiago@hotmail.com"
-#git config user.name "tiagonapoli"
+sudo apt-get -y install git
+git config --global user.email "napoli.tiago@hotmail.com"
+git config --global user.name "tiagonapoli"
 
 #ssh git
-#echo "ssh github"
-#rm -rf ~/.ssh/id_rsa ~/.ssh/id_rsa.pub
-#ssh-keygen -t rsa
-#ssh-add ~/.ssh/id_rsa
-#pub=`cat ~/.ssh/id_rsa.pub`
-#githubuser="tiagonapoli"
-#echo "using username $githubuser"
-#read -s -p "enter github password for user $githubuser: " githubpass
-#curl -u "$githubuser:$githubpass" -x post -d "{\"title\":\"`hostname`\",\"key\":\"$pub\"}" https://api.github.com/user/keys
+echo "ssh github"
+rm -rf ~/.ssh/id_rsa ~/.ssh/id_rsa.pub
+ssh-keygen -t rsa
+ssh-add ~/.ssh/id_rsa
+pub=`cat ~/.ssh/id_rsa.pub`
+read -p "Enter github username: " githubuser
+echo "Using username $githubuser"
+read -s -p "Enter github password for user $githubuser: " githubpass
+curl -u "$githubuser:$githubpass" -X POST -d "{\"title\":\"`hostname`\",\"key\":\"$pub\"}" https://api.github.com/user/keys
 
 #clone git
-#git clone git@github.com:tiagonapoli/maratona.git
+git clone git@github.com:tiagonapoli/maratona.git
 cp maratona/vimrc ~/.vimrc
 
 #tweaks
 sudo apt -y install gnome-tweaks
+sudo apt-get install chrome-gnome-shell
+
 
