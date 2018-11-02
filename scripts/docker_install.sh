7f07dffd5fb3ede48265eacd40b24871d353b56e
tiagonapoli <napoli.tiago@hotmail.com>
2018-11-01 23:31:38 -0300

    Refactor docker_install.sh
    Signed-off-by: tiagonapoli <napoli.tiago@hotmail.com>

--- scripts/docker_install.sh
+++ scripts/docker_install.sh
@@ -5,23 +5,31 @@ function install_docker {
   chmod +x get-docker.sh
   ./get-docker.sh
   sudo usermod -aG docker $USER
-  docker --version || echo "Docker instalation failed"
+  rm get-docker.sh
+  if ! docker --version; then
+    echo "Docker instalation failed"
+    exit 1
+  fi
 }
 
 function install_docker_compose {
   sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
-  docker-compose --version || echo "Docker-compose installation failed"
+  if ! docker-compose --version; then
+    echo "Docker-compose installation failed"
+    exit 1
+  fi
 }
 
 function install_docker_machine {
   base=https://github.com/docker/machine/releases/download/v0.14.0 &&
   curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
   sudo install /tmp/docker-machine /usr/local/bin/docker-machine
-  docker-machine version || echo "Docker-machine installation failed"
+  if ! docker-machine version; then
+    echo "Docker-machine installation failed"
+    exit 1
+  fi
 }
-
-cd ~/Documents
 
 #docker
 docker --version || install_docker
