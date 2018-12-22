01e8c3ec9cb0f4ade41acb6014def55cfa6a20ed
tiagonapoli <napoli.tiago@hotmail.com>
2018-12-22 12:00:44 -0200

    Removed input sources/natural scroll
    Reason: Input sources is bugging chrome Ctrl+W

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -4,7 +4,3 @@ gsettings set com.ubuntu.sound allow-amplified-volume true
 gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'google-chrome.desktop', 'code.desktop']"
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 20
-gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
-gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'br'), ('xkb', 'us')]"
-gsettings set org.gnome.desktop.input-sources mru-sources "[('xkb', 'us'), ('xkb', 'br')]"
-gsettings set org.gnome.desktop.input-sources xkb-options "[]"
